f49cd9864a374c76d05ba9d730ea6ccbd54bac4d
alite269 <alite269@gmail.com>
2013-10-19 15:18:54 +0800

    fix #7

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-(function(){
-	var canvas = document.getElementById('game');
+var gameModule = (function(){
+    var timeoutVar,counter=0;
+    function start () {
+    
+    var canvas = document.getElementById('game');
     var ctx = canvas.getContext('2d');
 
     var ballX = Math.floor(Math.random() * 300); // 0..300
@@ -13,4 +16,17 @@
     ctx.beginPath();
     ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
     ctx.fill();
+        if(counter>=10)
+        {
+        }else
+        {
+           timeoutVar = setTimeout(start,500);
+           counter ++;
+          counsole.log("Counter: "+ocunter);
+        }
+    }
+    return{
+        start:start
+    }
 })();
+gameModule.start();
